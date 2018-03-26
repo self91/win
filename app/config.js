faf8ea7f5c7f6d90a16c9d9819a04d8ee3a48140
tyler <tytytyler91@gmail.com>
2018-03-26 16:12:01 -0700

    Initial commit

---
+++ app/config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+exports.version = '0.1.0';
+exports.id = 'com.mybrowseraddon.node';
+
+exports.ids = {
+  "chrome": [
+    "enijlgojimhgkefecingdinhcfpinbhp",  /* opera: open-in-ie */
+    "anlkimmcefcceiddhggegodhkgkndhic",  /* opera: open-in-chrome */
+    "feinkgpipgaoonanlbgcpbkbjaggdaml",  /* opera: open-with-idm */
+    "dkhhkjpckbmkfdicijemmdaieaagkcco",  /* chrome: open-in-ie */
+    "dcfemfgnhfagfkeglchakeobdjdgcanl",  /* chrome: open-with-idm */
+    "nhpfgmjcpbdcbmhjbhogflbbmdbomfmb",  /* chrome: open-in-vlc */
+    "bfannejkbkejigonaemcmbbkpnfadlii",  /* opera: open-in-vlc */
+    "iabonojeikmegigkkgjmdicdciollhfk",  /* opera: open-with-pdf */
+    "ehndikigeekodlnbohjndjjiikphickb",  /* chrome: open-with-pdf */
+    "epgilldoipfbdlneomlmegekaoonchcj"   /* chrome: open-with-idm */
+  ],
+  "firefox": [
+    "jid1-q2yAXbZkz2GZH4@jetpack",             /* open-in-ie */
+    "jid1-r2yWXsZku2AJH2@jetpack",             /* open-in-chrome */
+    "{6c0194b2-e924-4987-b9a9-6836a5e46752}",  /* open-with-idm */
+    "{e427e22d-70e7-4132-8764-8bab93601197}",  /* open-in-vlc */
+    "{2582ab30-4fca-475f-88d0-c1a9b9ed978f}",  /* open-with-pdf */
+    "{9cd72ecf-3268-4966-a6e3-d82b2eeef4ad}"   /* open-with-idm */
+  ]
+};
